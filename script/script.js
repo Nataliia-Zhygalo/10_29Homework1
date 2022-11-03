899c069c8e0cd482d021df73a7de29b7ecb4c571
Nataliia <109826219+Nataliia-Zhygalo@users.noreply.github.com>
2022-11-03 17:47:38 -0600

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,13 +1,7 @@
 
-num1= parseInt(prompt ("Type in your first number"));
-num2 = parseInt(prompt ("Type in your second number"));
-console.log (num1,num2);
-add = num1 + num2;
-minus = num1 - num2;
-multiply = num1 * num2;
-devide = num1 / num2;
-alert (`You will get
-        ${num1} + ${num2} = ${add};
-        ${num1} - ${num2} = ${minus};
-        ${num1} * ${num2} = ${multiply};
-        ${num1} / ${num2} = ${devide};`);
+hours= parseInt(prompt("Type in any amount of hours?"));
+console.log(hours);
+seconds= hours * 60;
+alert(`There are ${seconds} seconds in ${hours} hours.`);
+
+
